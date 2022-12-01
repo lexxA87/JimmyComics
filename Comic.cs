c29e1a207c54b364c16ee349cdcc3c3fff90b222
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-01 12:51:51 +0400

    Added dictionary Prices for prices of comics

--- Comic.cs
+++ Comic.cs
@@ -18,5 +18,17 @@
             new Comic { Name = "Tribal Tattoo Madness", Issue = 83 },
             new Comic { Name = "The Death of the Object", Issue = 97 },
         };
+
+        public static readonly IReadOnlyDictionary<int, decimal> Prices = new Dictionary<int, decimal>
+        {
+            { 6, 3600M },
+            { 19, 500M },
+            { 36, 650M },
+            { 57, 13525M },
+            { 68, 250M },
+            { 74, 75M },
+            { 83, 25.75M },
+            { 97, 35.25M },
+        };
     }
 }
