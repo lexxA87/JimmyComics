9aa0e55be31bd1cbe55d4b02f4f5fc03e1eaf983
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-01 12:43:48 +0400

    Added class Comic

---
+++ Comic.cs
@@ -0,0 +1,10 @@
+﻿namespace JimmyComics
+{
+    internal class Comic
+    {
+        public string? Name { get; set; }
+        public int Issue { get; set; }
+
+        public override string ToString() => $"{Name} (Issue #{Issue})";
+    }
+}
