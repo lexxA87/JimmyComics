2cde9df9bf483113086dda832adb39335cb92027
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-01 12:47:10 +0400

    Added list of comics as a static field

--- Comic.cs
+++ Comic.cs
@@ -6,5 +6,17 @@
         public int Issue { get; set; }
 
         public override string ToString() => $"{Name} (Issue #{Issue})";
+
+        public static readonly IEnumerable<Comic> Catalog = new List<Comic>
+        {
+            new Comic { Name = "Johnny America vs. the Pinko", Issue = 6 },
+            new Comic { Name = "Rock and Roll (limited edition)", Issue = 19 },
+            new Comic { Name = "Woman's Work", Issue = 36 },
+            new Comic { Name = "Hippie Madness (misprinted)", Issue = 57 },
+            new Comic { Name = "Revenge of the New Wave Freak (damaged)", Issue = 68 },
+            new Comic { Name = "Black Monday", Issue = 74 },
+            new Comic { Name = "Tribal Tattoo Madness", Issue = 83 },
+            new Comic { Name = "The Death of the Object", Issue = 97 },
+        };
     }
 }
