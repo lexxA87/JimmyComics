2ea8c59591943718ff8cd28a592886b4613f473b
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-01 13:07:42 +0400

    Changed main method

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(string[] args)
         {
-            IEnumerable<Comic> mostExpensive =
+            IEnumerable<string> mostExpensive =
                 from comic in Comic.Catalog
                 where Comic.Prices[comic.Issue] > 500
-                orderby -Comic.Prices[comic.Issue]
-                select comic;
+                orderby Comic.Prices[comic.Issue] descending
+                select $"{comic} is worth {Comic.Prices[comic.Issue]:c}";
 
-            foreach (Comic comic in mostExpensive)
+            foreach (string comic in mostExpensive)
             {
-                Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:c}");
+                Console.WriteLine(comic);
             }
         }
     }
