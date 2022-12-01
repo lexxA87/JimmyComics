8b510a2c4c0ee48293283c19fd66c2f4033a1603
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-01 13:00:02 +0400

    Changed method main for print most expensive comics

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IEnumerable<Comic> mostExpensive =
+                from comic in Comic.Catalog
+                where Comic.Prices[comic.Issue] > 500
+                orderby -Comic.Prices[comic.Issue]
+                select comic;
+
+            foreach (Comic comic in mostExpensive)
+            {
+                Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:c}");
+            }
         }
     }
 }
